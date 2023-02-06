c6a8452001c4d2d14648aa394f457d5c926dd139
Sara Stanton <sara.mpa13@gmail.com>
2023-02-06 00:42:25 -0600

    Fix slider reset on refinement clear

--- src/Components/CustomRangeSlider.js
+++ src/Components/CustomRangeSlider.js
@@ -5,46 +5,47 @@ import { useEffect, useState } from 'react';
 import { connectRange } from 'react-instantsearch-dom'
 
 const RangeSlider = ({ defaultValues, currentRefinement, refine }) => {
-    const [value, setValue] = useState(null)
-    const [valueDisplay, setValueDisplay] = useState({ min: defaultValues.min, max: defaultValues.max })
-    const styles = {
-      sliderValues: {
-        display: 'flex',
-        justifyContent: 'space-between',
-        marginBottom: '5px'
-      }
+  const [value, setValue] = useState(null)
+  const [valueDisplay, setValueDisplay] = useState({ min: defaultValues.min, max: defaultValues.max })
+  const styles = {
+    sliderValues: {
+      display: 'flex',
+      justifyContent: 'space-between',
+      marginBottom: '5px'
     }
+  }
 
-    useEffect(() => {
-      if (value) {
-        const [min, max] = value
-        refine({ min, max })
-      }
-    }, [value, refine])
+  useEffect(() => {
+    if (value) {
+      const [min, max] = value
+      refine({ min, max })
+    }
+  }, [value, refine])
 
-    useEffect(() => {
-      if (!currentRefinement.min && currentRefinement.min !== 0  || !currentRefinement.max && currentRefinement.max !== 0 ) {
-        setValueDisplay({ min: defaultValues.min, max: defaultValues.max })
-      }
-    }, [currentRefinement, defaultValues.min, defaultValues.max])
+  useEffect(() => {
+    if (currentRefinement.min !== valueDisplay.min || currentRefinement.max !== valueDisplay.max ) {
+      setValueDisplay({ min: currentRefinement.min, max: currentRefinement.max })
+      setValue([currentRefinement.min, currentRefinement.max])
+    }
+  }, [currentRefinement])
 
-    return (
-        <>
-            <div style={styles.sliderValues}>
-              <span>{valueDisplay.min}</span>
-              <span>{valueDisplay.max}</span>
-            </div>
-            <Slider
-              range
-              defaultValue={ [defaultValues.min, defaultValues.max] }
-              onChange={v => setValueDisplay({ min: v[0], max: v[1] })}
-              onAfterChange={v => setValue(v)}
-              min={defaultValues.min}
-              max={defaultValues.max}
-              value={[valueDisplay.min, valueDisplay.max]}
-            />
-        </>
-    );
+  return (
+    <>
+      <div style={styles.sliderValues}>
+        <span>{valueDisplay.min}</span>
+        <span>{valueDisplay.max}</span>
+      </div>
+      <Slider
+        range
+        defaultValue={[defaultValues.min, defaultValues.max]}
+        onChange={v => setValueDisplay({ min: v[0], max: v[1] })}
+        onAfterChange={v => setValue(v)}
+        min={defaultValues.min}
+        max={defaultValues.max}
+        value={[valueDisplay.min, valueDisplay.max]}
+      />
+    </>
+  );
 };
 
 const CustomRangeSlider = connectRange(RangeSlider);

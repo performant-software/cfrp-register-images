c8858558cd26864da808ef633756daadbd7ff183
Rebecca Black <rebecca.ann.black@gmail.com>
2024-06-07 15:19:31 -0400

    Delete src directory

--- src/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import 'semantic-ui-css/semantic.min.css'
-import App from './App';
-
-createRoot(document.getElementById('root')).render(<App />);
